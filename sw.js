94ed9df92886391ebc5303a1cb6973dcdfb50794
swami-taporati <64623355+swami-taporati@users.noreply.github.com>
2026-02-16 16:04:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,22 @@
-const CACHE_NAME = 'heritage-v1';
-const ASSETS = [
+const CACHE_NAME = 'heritage-v3-cache';
+const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json',
-  './images/roller.png',
-  './images/elephant.png',
-  './images/lotus.png',
-  './images/sandalwood.png',
-  './images/mango.png'',
   'https://fonts.googleapis.com/css2?family=Cinzel:wght@700&family=Poppins:wght@400;600&display=swap'
 ];
 
 // Install Service Worker
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch Assets from Cache
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+// Network falling back to Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
